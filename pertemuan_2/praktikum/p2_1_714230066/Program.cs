b84101e7c1f26b1ee030f988cb6d2d642153c6bd
ZahraDH28 <diahandokozahra29@gmail.com>
2024-10-15 16:28:59 +0700

    Pertemuan ke 2

---
+++ pertemuan_2/praktikum/p2_1_714230066/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P2_1_714230066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukan angka pertama: ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("masukan angka kedua: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine($"{input1} + {input2} = {input1 + input2}");
+            Console.WriteLine($"{input1} - {input2} = {input1 - input2}");
+            Console.WriteLine($"{input1} * {input2} = {input1 * input2}");
+            Console.WriteLine($"{input1} / {input2} = {input1 / input2}");
+
+
+        }
+    }
+}
