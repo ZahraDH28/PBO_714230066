22d0fcb5bafcd868ba552ef609d6cca9ca0179bb
ZahraDH28 <diahandokozahra29@gmail.com>
2024-11-19 15:59:03 +0700

    tugas

---
+++ Pertemuan_7/Praktikum/P7_1_714230066/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace P7_1_714230066
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxNama = new System.Windows.Forms.TextBox();
+            this.TextBoxKelas = new System.Windows.Forms.TextBox();
+            this.comboBoxAngkatan = new System.Windows.Forms.ComboBox();
+            this.buttonCek = new System.Windows.Forms.Button();
+            this.buttonTutup = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxTidur = new System.Windows.Forms.CheckBox();
+            this.checkBoxKuliah = new System.Windows.Forms.CheckBox();
+            this.radioButtonWeekday = new System.Windows.Forms.RadioButton();
+            this.radioButtonWeekend = new System.Windows.Forms.RadioButton();
+            this.checkBoxLiburan = new System.Windows.Forms.CheckBox();
+            this.buttonCetak = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angkatan :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelas :";
+            // 
+            // TextBoxNama
+            // 
+            this.TextBoxNama.Location = new System.Drawing.Point(121, 12);
+            this.TextBoxNama.Name = "TextBoxNama";
+            this.TextBoxNama.Size = new System.Drawing.Size(269, 22);
+            this.TextBoxNama.TabIndex = 3;
+            this.TextBoxNama.TextChanged += new System.EventHandler(this.TextBoxNama_TextChanged);
+            // 
+            // TextBoxKelas
+            // 
+            this.TextBoxKelas.Location = new System.Drawing.Point(121, 90);
+            this.TextBoxKelas.Multiline = true;
+            this.TextBoxKelas.Name = "TextBoxKelas";
+            this.TextBoxKelas.Size = new System.Drawing.Size(269, 59);
+            this.TextBoxKelas.TabIndex = 5;
+            // 
+            // comboBoxAngkatan
+            // 
+            this.comboBoxAngkatan.FormattingEnabled = true;
+            this.comboBoxAngkatan.Items.AddRange(new object[] {
+            "2020",
+            "2021",
+            "2022",
+            "2023",
+            "2024"});
+            this.comboBoxAngkatan.Location = new System.Drawing.Point(121, 50);
+            this.comboBoxAngkatan.Name = "comboBoxAngkatan";
+            this.comboBoxAngkatan.Size = new System.Drawing.Size(269, 24);
+            this.comboBoxAngkatan.TabIndex = 6;
+            // 
+            // buttonCek
+            // 
+            this.buttonCek.Location = new System.Drawing.Point(234, 168);
+            this.buttonCek.Name = "buttonCek";
+            this.buttonCek.Size = new System.Drawing.Size(75, 23);
+            this.buttonCek.TabIndex = 7;
+            this.buttonCek.Text = "Cek";
+            this.buttonCek.UseVisualStyleBackColor = true;
+            this.buttonCek.Click += new System.EventHandler(this.buttonCek_Click);
+            // 
+            // buttonTutup
+            // 
+            this.buttonTutup.Location = new System.Drawing.Point(315, 168);
+            this.buttonTutup.Name = "buttonTutup";
+            this.buttonTutup.Size = new System.Drawing.Size(75, 23);
+            this.buttonTutup.TabIndex = 8;
+            this.buttonTutup.Text = "Tutup";
+            this.buttonTutup.UseVisualStyleBackColor = true;
+            this.buttonTutup.Click += new System.EventHandler(this.buttonTutup_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.No;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 32);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "KEGIATAN";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // checkBoxTidur
+            // 
+            this.checkBoxTidur.AutoSize = true;
+            this.checkBoxTidur.Location = new System.Drawing.Point(14, 432);
+            this.checkBoxTidur.Name = "checkBoxTidur";
+            this.checkBoxTidur.Size = new System.Drawing.Size(60, 20);
+            this.checkBoxTidur.TabIndex = 10;
+            this.checkBoxTidur.Text = "Tidur";
+            this.checkBoxTidur.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKuliah
+            // 
+            this.checkBoxKuliah.AutoSize = true;
+            this.checkBoxKuliah.Location = new System.Drawing.Point(14, 391);
+            this.checkBoxKuliah.Name = "checkBoxKuliah";
+            this.checkBoxKuliah.Size = new System.Drawing.Size(65, 20);
+            this.checkBoxKuliah.TabIndex = 11;
+            this.checkBoxKuliah.Text = "Kuliah";
+            this.checkBoxKuliah.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWeekday
+            // 
+            this.radioButtonWeekday.AutoSize = true;
+            this.radioButtonWeekday.Location = new System.Drawing.Point(14, 286);
+            this.radioButtonWeekday.Name = "radioButtonWeekday";
+            this.radioButtonWeekday.Size = new System.Drawing.Size(102, 20);
+            this.radioButtonWeekday.TabIndex = 12;
+            this.radioButtonWeekday.TabStop = true;
+            this.radioButtonWeekday.Text = "Senin-Jumat";
+            this.radioButtonWeekday.UseVisualStyleBackColor = true;
+            this.radioButtonWeekday.CheckedChanged += new System.EventHandler(this.radioButtonWeekday_CheckedChanged);
+            // 
+            // radioButtonWeekend
+            // 
+            this.radioButtonWeekend.AutoSize = true;
+            this.radioButtonWeekend.Location = new System.Drawing.Point(14, 324);
+            this.radioButtonWeekend.Name = "radioButtonWeekend";
+            this.radioButtonWeekend.Size = new System.Drawing.Size(111, 20);
+            this.radioButtonWeekend.TabIndex = 13;
+            this.radioButtonWeekend.TabStop = true;
+            this.radioButtonWeekend.Text = "Sabtu-Minggu";
+            this.radioButtonWeekend.UseVisualStyleBackColor = true;
+            this.radioButtonWeekend.CheckedChanged += new System.EventHandler(this.radioButtonWeekend_CheckedChanged);
+            // 
+            // checkBoxLiburan
+            // 
+            this.checkBoxLiburan.AutoSize = true;
+            this.checkBoxLiburan.Location = new System.Drawing.Point(14, 468);
+            this.checkBoxLiburan.Name = "checkBoxLiburan";
+            this.checkBoxLiburan.Size = new System.Drawing.Size(73, 20);
+            this.checkBoxLiburan.TabIndex = 14;
+            this.checkBoxLiburan.Text = "Liburan";
+            this.checkBoxLiburan.UseVisualStyleBackColor = true;
+            // 
+            // buttonCetak
+            // 
+            this.buttonCetak.Location = new System.Drawing.Point(234, 507);
+            this.buttonCetak.Name = "buttonCetak";
+            this.buttonCetak.Size = new System.Drawing.Size(75, 23);
+            this.buttonCetak.TabIndex = 15;
+            this.buttonCetak.Text = "Cetak";
+            this.buttonCetak.UseVisualStyleBackColor = true;
+            this.buttonCetak.Click += new System.EventHandler(this.buttonCetak_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(315, 507);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 16;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 542);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonCetak);
+            this.Controls.Add(this.checkBoxLiburan);
+            this.Controls.Add(this.radioButtonWeekend);
+            this.Controls.Add(this.radioButtonWeekday);
+            this.Controls.Add(this.checkBoxKuliah);
+            this.Controls.Add(this.checkBoxTidur);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonTutup);
+            this.Controls.Add(this.buttonCek);
+            this.Controls.Add(this.comboBoxAngkatan);
+            this.Controls.Add(this.TextBoxKelas);
+            this.Controls.Add(this.TextBoxNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBoxNama;
+        private System.Windows.Forms.TextBox TextBoxKelas;
+        private System.Windows.Forms.ComboBox comboBoxAngkatan;
+        private System.Windows.Forms.Button buttonCek;
+        private System.Windows.Forms.Button buttonTutup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxTidur;
+        private System.Windows.Forms.CheckBox checkBoxKuliah;
+        private System.Windows.Forms.RadioButton radioButtonWeekday;
+        private System.Windows.Forms.RadioButton radioButtonWeekend;
+        private System.Windows.Forms.CheckBox checkBoxLiburan;
+        private System.Windows.Forms.Button buttonCetak;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Pertemuan_7/Praktikum/P7_1_714230066/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P7_1_714230066
+{
+    public partial class Form1 : Form
+    {
+        public object ComboBoxAngkatan { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Size = new Size(330, 230);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxNama_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCek_Click(object sender, EventArgs e)
+        {
+            StringBuilder errorMessage = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(TextBoxNama.Text))
+            {
+                errorMessage.AppendLine("Nama Harus Diisi");
+            }
+
+            if (comboBoxAngkatan.SelectedIndex == -1)
+            {
+                errorMessage.AppendLine("Angkatan Harus Diisi");
+            }
+
+            if (string.IsNullOrWhiteSpace(TextBoxKelas.Text))
+            {
+                errorMessage.AppendLine("Kelas Harus Diisi");
+            }
+
+            string errorMsg = errorMessage.ToString();
+
+            if (string.IsNullOrEmpty(errorMsg))
+            {
+                MessageBox.Show(
+                "Lengkap!!",
+                "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Size = new Size(330, 500);
+            }
+
+            else
+            {
+                MessageBox.Show(
+                errorMsg.Trim(),
+                "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+
+
+        private void buttonTutup_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void radioButtonWeekday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonWeekday.Checked)
+            {
+                checkBoxKuliah.Enabled = true; checkBoxKuliah.Checked = false;
+                checkBoxTidur.Enabled = true; checkBoxTidur.Checked = false;
+                checkBoxLiburan.Enabled = false; checkBoxLiburan.Checked = false;
+            }
+
+        }
+
+        private void radioButtonWeekend_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxKuliah.Enabled = false; checkBoxKuliah.Checked = false;
+            checkBoxTidur.Enabled = true; checkBoxTidur.Checked= false;
+            checkBoxLiburan.Enabled = true; checkBoxLiburan.Checked = false;
+        }
+
+        private void buttonCetak_Click(object sender, EventArgs e)
+        {
+            string hari = null;
+            string kegiatan = null;
+
+            foreach (Control control in Controls)
+            {
+                if (control is RadioButton radioButton && radioButton.Checked)
+                {
+                    hari = radioButton.Text;
+                    break;
+                }
+            }
+
+            foreach (Control control in Controls)
+            {
+                if (control is CheckBox checkBox && checkBox.Checked)
+                {
+                    if (!string.IsNullOrEmpty(kegiatan))
+                    {
+                        kegiatan += ",";
+                    }
+                    kegiatan += checkBox.Text;
+                }
+
+
+            }
+            MessageBox.Show(
+                    "Nama: " + TextBoxNama.Text + "\n" +
+                    "Angkatan: " + comboBoxAngkatan.Text + "\n" +
+                    "Kelas: " + TextBoxKelas.Text + "\n" +
+                    "=======================================\n" +
+                    "Hari: " + hari + "\n" +
+                    "Kegiatan: " + kegiatan + "\n",
+                    "Informasi Data Submit",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
+
+            ////menggunakan LINQ 
+            //string hari = Controls.OfType<RadioButton>()
+            //    .FirstOrDefault(rb => rb.Checked)?.Text;
+
+            //string kegiatan = string.Join(",",
+            //    Controls.OfType<CheckBox>()?
+            //    .Where(cb => cb.Checked)
+            //    .Select(cb => cb.Text));
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            //TextBoxNama.Text = "";
+            //comboBoxAngkatan.SelectedIndex = -1;
+            //TextBoxKelas.Text = "";
+            //radioButtonWeekday.Checked = false;
+            //radioButtonWeekend.Checked = false;
+            //checkBoxKuliah.Checked = false;
+            //checkBoxLiburan.Checked = false;
+            //checkBoxTidur.Checked = false;
+            Size = new Size(330, 230);
+
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.Text = "";
+                }
+
+                else if (control is ComboBox comboBox)
+                {
+                    comboBox.SelectedIndex = -1;
+                }
+
+                else if (control is RadioButton radioButton)
+                {
+                    radioButton.Checked = false;
+                }
+
+                else if (control is CheckBox checkBox)
+                {
+                    checkBox.Checked = false;
+                    checkBox.Enabled = true;
+                }
+            }
+
+                    MessageBox.Show(
+                    "Berhasil Di Reset",
+                    "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
+         
+        }
+    }
+}
